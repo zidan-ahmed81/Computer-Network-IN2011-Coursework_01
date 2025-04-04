ab7e9d135bc9f6fdc0d90fc4a1ab38dcb7ea6dee
zidanahmed81 <158052715+zidanahmed81@users.noreply.github.com>
2025-04-04 04:17:16 +0100

    First

--- src/AzureLabTest.java
+++ src/AzureLabTest.java
@@ -92,9 +92,7 @@ class AzureLabTest {
             }
 
 
-            System.out.println("Checking if 'D:jabberwocky0' exists...");
-            boolean exists = node.exists("D:jabberwocky0");
-            System.out.println("Result: " + exists);
+            boolean exists = (node.read("D:jabberwocky0") != null);
 
             // Finally we will let other nodes know where we are
             // so that we can be contacted and can store data for others.
