dff214d429366bc91d547af84c5ac4824c7c2039
zidanahmed81 <158052715+zidanahmed81@users.noreply.github.com>
2025-04-04 05:14:08 +0100

    First

--- src/AzureLabTest.java
+++ src/AzureLabTest.java
@@ -24,20 +24,11 @@ import java.util.Date;
 import java.util.Random;
 
 class AzureLabTest {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         String emailAddress = "zidan.ahmed.2@city.ac.uk";
-        if (false && emailAddress.indexOf('@') == -1) {
-            System.err.println("Please set your e-mail address!");
-            System.exit(1);
-        }
         String ipAddress = "10.216.34.203";
-        if (false && ipAddress.indexOf('.') == -1) {
-            System.err.println("Please set your ip address!");
-            System.exit(1);
-        }
 
         try {
-            // Create a node and initialise it
             Node node = new Node();
             String nodeName = "N:" + emailAddress;
             node.setNodeName(nodeName);
@@ -51,21 +42,13 @@ class AzureLabTest {
             node.pushRelay("N:blue");
             node.pushRelay("N:yellow");
             node.pushRelay("N:orange");
-            node.popRelay(); // should remove N:orange
-            node.popRelay(); // should remove N:yellow
-            node.popRelay(); // should remove N:blue
-            node.popRelay(); // should print that stack is empty
+            node.popRelay();
+            node.popRelay();
+            node.popRelay();
+            node.popRelay();
 
-
-
-
-
-            // Wait and hope that we get sent the address of some other nodes
             System.out.println("Waiting for another node to get in contact");
             node.handleIncomingMessages(15 * 1000);
-
-            // Let's start with a test of reading key/value pairs stored on the network.
-            // This should print out a poem.
 
             System.out.println("Getting the poem...");
             for (int i = 0; i < 7; ++i) {
@@ -73,24 +56,18 @@ class AzureLabTest {
                 String value = node.read(key);
                 if (value == null) {
                     System.err.println("Can't find poem verse " + i);
-//                    System.exit(2);
                 } else {
                     System.out.println(value);
                 }
             }
 
-
-            // Now let's test writing a key/value pair
             System.out.println("Writing a marker so it's clear my code works");
             {
                 String key = "D:" + emailAddress;
                 String value = "It works!";
                 boolean success = node.write(key, value);
-
-                // Read it back to be sure
                 System.out.println(node.read(key));
             }
-
 
             System.out.println("Checking if D:jabberwocky0 exists...");
             boolean exists = node.exists("D:jabberwocky0");
@@ -101,24 +78,14 @@ class AzureLabTest {
             node.write(key, "alpha");
 
             boolean ok = node.CAS(key, "alpha", "beta");
-            System.out.println("CAS(alpha → beta): " + ok);  // should be true if value is alpha
+            System.out.println("CAS(alpha → beta): " + ok);
 
             String result = node.read(key);
-            System.out.println("Result: " + result);  // should show beta
-
-            // Finally we will let other nodes know where we are
-            // so that we can be contacted and can store data for others.
-            System.out.println("Letting other nodes know where we are");
-            node.write(nodeName, ipAddress + ":" + port);
-
-            System.out.println("Handling incoming connections");
-            node.handleIncomingMessages(1000);
-
+            System.out.println("Result: " + result);
 
         } catch (Exception e) {
             System.err.println("Exception during AzureLabTest");
             e.printStackTrace(System.err);
-            return;
         }
     }
 }
